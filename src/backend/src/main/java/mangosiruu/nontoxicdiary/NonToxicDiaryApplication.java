7180f0ea1269330a44ce727a777761478acad876
Jo HyunA <teniuscom@gmail.com>
2024-07-15 20:26:38 +0900

    [init] springboot project

---
+++ src/backend/src/main/java/mangosiruu/nontoxicdiary/NonToxicDiaryApplication.java
@@ -0,0 +1,13 @@
+package mangosiruu.nontoxicdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonToxicDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonToxicDiaryApplication.class, args);
+	}
+
+}
