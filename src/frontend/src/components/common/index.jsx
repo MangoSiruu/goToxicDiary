4399b2a4d8930e8c647b00b5d09bc4c50b1de9c3
yeahzee0421 <ydw08250421@gmail.com>
2024-07-19 17:09:27 +0900

    [chore] #1 - 폴더 설명 추가

---
+++ src/frontend/src/components/common/index.jsx
@@ -0,0 +1,8 @@
+/*
+common 폴더: 공통적으로 여러 곳에서 사용될만한 컴포넌트를 저장합니다.
+예시) Button, Form, Image, layouts 등
+폴더 생성 후 index.jsx에 구현해주시면 됩니다 :D
+📦common
+ ┣ 📂Button
+ ┃ ┗ 📜index.jsx
+ */
---
+++ src/frontend/src/components/features/index.jsx
@@ -0,0 +1,8 @@
+/*
+features 폴더: 기능별 컴포넌트 저장
+예시) MyCalendar, Challenge, MyAccount 등
+폴더 생성 후 index.jsx에 구현해주시면 됩니다 :D
+📦features
+ ┣ 📂Challenge
+ ┃ ┗ 📜index.jsx
+ */
