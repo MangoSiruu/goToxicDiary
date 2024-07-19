55c42ffcbbd87cabbc4c716dce753d891e160eb5
yeahzee0421 <ydw08250421@gmail.com>
2024-07-19 14:58:50 +0900

    [init] #1 - Eslint, Prettier 세팅

--- src/frontend/.eslintrc.cjs
+++ src/frontend/.eslintrc.cjs
@@ -1,21 +1,36 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-}
+  settings: { react: { version: '18.2' } },
+  plugins: ['react', 'react-refresh', '@emotion', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'react/jsx-no-target-blank': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'import/extensions': ['off'],
+    'import/no-extraneous-dependencies': ['off'],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+};
