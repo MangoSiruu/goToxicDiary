249b2bd656b59a586395b84525997e51e5b52c8e
leumi802 <duswl040802@gmail.com>
2024-07-26 19:16:32 +0900

    [design] #6 배경색 투명도 적용

--- src/frontend/src/components/common/Categories/index.jsx
+++ src/frontend/src/components/common/Categories/index.jsx
@@ -1,83 +1,58 @@
 import React from "react";
 import styled from "@emotion/styled"
-import { colors } from "../../../styles/variants"
-
-// const CategoryBox = (props) => {
-    
-//     return (
-//         <Wrapper>
-
-//         </Wrapper>
-//     );
-// };
-
-// const Wrapper = styled.div({
-
-    
-
-// })
 
 const categoryStyles = {
+  // category type - icon, backgroundColor 매핑
     beer: {
       icon: '🍺',
-      backgroundColor: colors.point_blue,
+      backgroundColor: '66, 158, 189, 0.7',
     },
     coffee: {
       icon: '☕',
-      backgroundColor: colors.point_mint,
+      backgroundColor: '0, 161, 157, 0.7',
     },
     cola: {
       icon: '🥤',
-      backgroundColor: colors.point_orange,
+      backgroundColor: '252, 159, 102, 0.7',
     },
     instant: {
       icon: '🍔',
-      backgroundColor: colors.point_green,
+      backgroundColor: '0, 161, 81, 0.7',
     },
     pepper: {
       icon: '🌶️',
-      backgroundColor: colors.point_pink,
+      backgroundColor: '224, 93, 93, 0.7',
     },
     pizza: {
       icon: '🍕',
-      backgroundColor: colors.point_navy,
+      backgroundColor: '5, 63, 92, 0.7',
     },
     spoon: {
       icon: '🍴',
-      backgroundColor: colors.mainOrange,
+      backgroundColor: '242, 127, 12, 0.7',
     },
   };
   
   // 카테고리 박스의 스타일을 정의한 컴포넌트
-  const Box = styled.div
-  // 배경 색 바뀜
-
-    // 높이 동일
-    // 넓이 동일
-    // center 동일
-    // padding 동일
-    // 폰트? 동일
-    // radius 동일
-  `
-    width: 90px;
-    height: 27px;
-    padding: 5px 10px;
+  const Box = styled.div`
     display: flex;
-    text-align: center;
+    flex-direction: row;
     justify-content: center;
     align-items: center;
-    border-radius: 8px;
-    background-color: ${(props) => props.bgColor};
+    padding: 5px 10px;
+    gap: 10px;
+
+    width: 90px;
+    height: 27px;
+
+    border-radius: 5px;
+    text-align: center;
+    background: rgba(${(props) => props.bgColor});
     color: white;
   `;
   
   // CategoryBox 컴포넌트 정의
   const CategoryBox = (props) => {
-    // 아이콘 바뀜
-    // count 바뀜
-
-    // type -> icon, backgroundColor 지정하고 싶음
-
     const { icon, backgroundColor } = categoryStyles[props.type];
   
     return (
