69c29427303ded1694c6fe02be54235c8a0d5478
yeahzee0421 <ydw08250421@gmail.com>
2024-07-26 20:12:05 +0900

    [feat] #12 - 게시글 post 로직 추가

--- src/frontend/src/api/instance/index.js
+++ src/frontend/src/api/instance/index.js
@@ -1,31 +1,15 @@
-import { QueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 
-const initInstance = () => {
-  const instance = axios.create({
-    timeout: 5000,
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  });
-
-  return instance;
-};
+export const axiosInstance = axios.create({
+  timeout: 5000,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
 
 export const BASE_URL = 'http://localhost:3000';
 
-export const fetchInstance = initInstance({
+export const fetchInstance = axiosInstance({
   baseURL: 'http://localhost:3000',
-});
-
-export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      refetchOnMount: true,
-      refetchOnReconnect: true,
-      refetchOnWindowFocus: true,
-    },
-  },
 });
