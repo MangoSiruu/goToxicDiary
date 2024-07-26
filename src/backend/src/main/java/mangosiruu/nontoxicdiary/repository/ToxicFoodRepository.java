cba6509d80975315818e3a3b18b5e9fca56a7cc7
seoyoungjin23 <ascd050@gmail.com>
2024-07-26 16:12:31 +0900

    fix: repository 쿼리문 수정

--- src/backend/src/main/java/mangosiruu/nontoxicdiary/repository/ToxicFoodRepository.java
+++ src/backend/src/main/java/mangosiruu/nontoxicdiary/repository/ToxicFoodRepository.java
@@ -13,27 +13,30 @@ public interface ToxicFoodRepository extends JpaRepository<ToxicFood, Long> {
 
     void deleteByDateAndUserInfo(LocalDate date, UserInfo userInfo);
 
-    List<ToxicFood> findByDateAndUserInfo (LocalDate date, UserInfo userInfo);
+    List<ToxicFood> findByDateAndUserInfo(LocalDate date, UserInfo userInfo);
 
-    List<ToxicFood> findByDateBetweenAndUserInfo(LocalDate startDate, LocalDate endDate, UserInfo userInfo);
+    List<ToxicFood> findByDateBetweenAndUserInfo(LocalDate startDate, LocalDate endDate,
+        UserInfo userInfo);
 
-    List<ToxicFood> findByDateBetweenAndCategoryFoodAndUserInfo(LocalDate startDate, LocalDate endDate,
+    List<ToxicFood> findByDateBetweenAndCategoryFoodAndUserInfo(LocalDate startDate,
+        LocalDate endDate,
         String filterCategory, UserInfo userInfo);
 
-    @Query("SELECT tf FROM ToxicFood tf WHERE YEAR(tf.date) = :year AND MONTH(tf.date) = :month")
-    List<ToxicFood> findByYearAndMonthAndUserInfo(@Param("year") int year, @Param("month") int month,
-        UserInfo userInfo);
-    
-    
+    @Query("SELECT tf FROM ToxicFood tf WHERE YEAR(tf.date) = :year AND MONTH(tf.date) = :month AND tf.userInfo = :userInfo")
+    List<ToxicFood> findByYearAndMonthAndUserInfo(@Param("year") int year,
+        @Param("month") int month,
+        @Param("userInfo") UserInfo userInfo);
+
+
     @Query("SELECT t.date " +
-            "FROM ToxicFood t " +
-            "WHERE t.userInfo.id=:userId " +
-                "AND t.category.id=:categoryId " +
-                "AND t.date BETWEEN :startDate AND :endDate " +
-                "AND t.count > :maxCount")
+        "FROM ToxicFood t " +
+        "WHERE t.userInfo.id=:userId " +
+        "AND t.category.id=:categoryId " +
+        "AND t.date BETWEEN :startDate AND :endDate " +
+        "AND t.count > :maxCount")
     List<LocalDate> findChallengeFailedDates(@Param("userId") Long userId,
-                                              @Param("categoryId") Long categoryId,
-                                              @Param("startDate") LocalDate startDate,
-                                              @Param("endDate") LocalDate endDate,
-                                              @Param("maxCount") Long maxCount);
+        @Param("categoryId") Long categoryId,
+        @Param("startDate") LocalDate startDate,
+        @Param("endDate") LocalDate endDate,
+        @Param("maxCount") Long maxCount);
 }
