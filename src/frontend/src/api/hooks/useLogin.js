22a9f737c94fa4d414a84d1dc4e50621408e2975
yeahzee0421 <ydw08250421@gmail.com>
2024-07-30 18:30:42 +0900

    [feat] #3 - 로그인 로직 추가

--- src/frontend/src/api/hooks/useLogin.js
+++ src/frontend/src/api/hooks/useLogin.js
@@ -2,20 +2,19 @@ import { useMutation } from '@tanstack/react-query';
 import { axiosInstance } from '../instance';
 import { endpoint } from '../path';
 
-const signUpRequest = async (data) => {
-  const response = await axiosInstance.post(`${endpoint.AUTH}/signup`, {
+const loginRequest = async (data) => {
+  const response = await axiosInstance.post(`${endpoint.AUTH}/login`, {
     username: data.id,
     password: data.password,
-    nickname: data.nickname,
   });
   return response.data;
 };
 
 const useSignUp = () => {
   return useMutation({
-    mutationFn: signUpRequest,
+    mutationFn: loginRequest,
     onSuccess: () => {
-      // store에 저장
+      // 토큰 저장
     },
   });
 };
