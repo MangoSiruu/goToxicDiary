d6aab583f8a0da817f639b3e97335aafb2da18bc
Dong-Han <donghk77@naver.com>
2024-08-03 20:35:25 +0900

    [feat] #24 - APP
    - app.jsx 추가

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,5 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import ChallengeDetailView from "./components/features/ChallengeDetailView/ChallengeDetailView";
+import MyChallengeListView from './components/features/MyChallengeListView/MyChallengeListView';
+import NewMyChallengeView from './components/features/NewMyChallengeView/NewMyChallengeView';
+import Login from './utils/FakeLogin/Login';
+
+
 function App() {
-  return <div>h1</div>;
+  return (
+    <div style={{ backgroundColor: '#FBF4EE' }}>
+    
+    <Login/>
+    
+    <Router>
+      <Routes>
+        <Route path="/mychallengelistview" element={<MyChallengeListView />} />
+        <Route path="/newmychallengeview" element={<NewMyChallengeView />} />
+        <Route path="/challengedetailview/:id" element={<ChallengeDetailView />} />
+      </Routes>
+    </Router>
+    </div>
+  );
 }
 
 export default App;
