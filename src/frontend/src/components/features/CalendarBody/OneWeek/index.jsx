460aad1ac3fcccb2111a47e065a39280d6e308b4
leumi802 <duswl040802@gmail.com>
2024-08-05 19:14:38 +0900

    [feat] #31 - OneWeek 동작 구현

--- src/frontend/src/components/features/CalendarBody/OneWeek/index.jsx
+++ src/frontend/src/components/features/CalendarBody/OneWeek/index.jsx
@@ -2,18 +2,102 @@ import React from "react";
 import OneDay from "../OneDay";
 import styled from "@emotion/styled";
 import { colors } from "../../../../styles/variants";
+import { format, add } from "date-fns";
 
-const OneWeek = () => {
+// <OneWeek sundayDate="2024-08-04" filter=""></OneWeek>
+const OneWeek = ({ sundayDate, filter }) => {
+    // 무조건 7일 기록 받아오기
+    const response = {
+        "message" : "섭취 기록 리스트 조회 성공",
+        "dailyRecords" : [
+            {
+                "date" : "2024-08-04",
+                "toxicFoods" : [
+                    {"name" : "술", "count" : 1},
+                    {"name" : "카페인", "count" : 2}
+                ],
+                "challengeSuccess" : [
+                    {"category":"카페인", "success" : true},
+                    {"category":"슬", "success" : false}
+                ]
+            },
+            {
+                "date" : "2024-08-05",
+                "toxicFoods" : [
+                    {"name" : "매운음식", "count" : 2},
+                    {"name" : "야식", "count" : 1}
+                ],
+                "challengeSuccess" : [
+                    {"category":"카페인", "success" : true},
+                    {"category":"슬", "success" : false}
+                ]
+            },
+            {
+                "date" : "2024-08-06",
+                "toxicFoods" : [
+                    {"name" : "매운음식", "count" : 2},
+                    {"name" : "야식", "count" : 1}
+                ],
+                "challengeSuccess" : [
+                    {"category":"카페인", "success" : true},
+                    {"category":"슬", "success" : false}
+                ]
+            },
+            {
+                "date" : "2024-08-07",
+                "toxicFoods" : [
+                ],
+                "challengeSuccess" : [
+                    {"category":"카페인", "success" : true},
+                    {"category":"슬", "success" : false}
+                ]
+            },
+            {
+                "date" : "2024-08-08",
+                "toxicFoods" : [
+                    {"name" : "매운음식", "count" : 2},
+                    {"name" : "야식", "count" : 1}
+                ],
+                "challengeSuccess" : [
+                    {"category":"카페인", "success" : true},
+                    {"category":"슬", "success" : false}
+                ]
+            },
+            {
+                "date" : "2024-08-09",
+                "toxicFoods" : [
+                    {"name" : "매운음식", "count" : 2},
+                    {"name" : "야식", "count" : 1}
+                ],
+                "challengeSuccess" : [
+                    {"category":"카페인", "success" : true},
+                    {"category":"슬", "success" : false}
+                ]
+            },
+            {
+                "date" : "2024-08-10",
+                "toxicFoods" : [
+                    {"name" : "매운음식", "count" : 2},
+                    {"name" : "야식", "count" : 1}
+                ],
+                "challengeSuccess" : [
+                    {"category":"카페인", "success" : true},
+                    {"category":"슬", "success" : false}
+                ]
+            }
+        ]
+    }
+
     return (
         <CalendarBody>
             <Wrapper>
-                <OneDay dateStr={"2024-07-21"}></OneDay>
-                <OneDay dateStr={"2024-07-22"}></OneDay>
-                <OneDay dateStr={"2024-07-23"}></OneDay>
-                <OneDay dateStr={"2024-07-24"}></OneDay>
-                <OneDay dateStr={"2024-07-25"}></OneDay>
-                <OneDay dateStr={"2024-07-26"}></OneDay>
-                <OneDay dateStr={"2024-07-27"}></OneDay>
+                {response.dailyRecords.map((record, index) => (
+                    <OneDay 
+                        key={index}
+                        dateStr={record.date}
+                        dailyResponse={record}>
+                    </OneDay>
+                ))}
             </Wrapper>
         </CalendarBody>
     );
